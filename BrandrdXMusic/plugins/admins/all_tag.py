349d19185f2c30a7ba2ed6e315ff3eea82059609
nobitasir251-blip <nobitasir251@gmail.com>
2026-05-26 21:00:44 +0530

    Update all_tag.py

--- BrandrdXMusic/plugins/admins/all_tag.py
+++ BrandrdXMusic/plugins/admins/all_tag.py
@@ -2,11 +2,11 @@ import asyncio
 import random
 from pyrogram import Client, filters
 from pyrogram.enums import ChatType, ChatMemberStatus
-from pyrogram.errors import UserNotParticipant
+from pyrogram.errors import UserNotParticipant, FloodWait
 from pyrogram.types import ChatPermissions
+
 from BrandrdXMusic import app
 from BrandrdXMusic.utils.branded_ban import admin_filter
-
 
 SPAM_CHATS = []
 
@@ -18,52 +18,77 @@ SPAM_CHATS = []
 async def tag_all_users(_, message):
 
     replied = message.reply_to_message
+
     if len(message.command) < 2 and not replied:
-        await message.reply_text(
-            "** ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ, ʟɪᴋᴇ »** `@all Hi Friends`"
+        return await message.reply_text(
+            "**ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ, ʟɪᴋᴇ »** `@all Hi Friends`"
         )
-        return
+
+    SPAM_CHATS.append(message.chat.id)
+
+    usernum = 0
+    usertxt = ""
+
     if replied:
-        SPAM_CHATS.append(message.chat.id)
-        usernum = 0
-        usertxt = ""
+
         async for m in app.get_chat_members(message.chat.id):
+
             if message.chat.id not in SPAM_CHATS:
                 break
+
             usernum += 1
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})"
+
             if usernum == 5:
-                await replied.reply_text(usertxt)
-                await asyncio.sleep(2)
+
+                try:
+                    await replied.reply_text(usertxt)
+                    await asyncio.sleep(3)
+
+                except FloodWait as e:
+                    await asyncio.sleep(e.value)
+
+                except Exception:
+                    pass
+
                 usernum = 0
                 usertxt = ""
-        try:
-            SPAM_CHATS.remove(message.chat.id)
-        except Exception:
-            pass
+
     else:
+
         text = message.text.split(None, 1)[1]
 
-        SPAM_CHATS.append(message.chat.id)
-        usernum = 0
-        usertxt = ""
         async for m in app.get_chat_members(message.chat.id):
+
             if message.chat.id not in SPAM_CHATS:
                 break
+
             usernum += 1
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})"
+
             if usernum == 5:
-                await app.send_message(
-                    message.chat.id,
-                    f"{text}\n{usertxt}\n\n|| ➥ ᴏғғ ᴛᴀɢɢɪɴɢ ʙʏ » /cancel ||",
-                )
-                await asyncio.sleep(2)
+
+                try:
+                    await app.send_message(
+                        message.chat.id,
+                        f"{text}\n{usertxt}\n\n|| ➥ ᴏғғ ᴛᴀɢɢɪɴɢ ʙʏ » /cancel ||",
+                    )
+
+                    await asyncio.sleep(3)
+
+                except FloodWait as e:
+                    await asyncio.sleep(e.value)
+
+                except Exception:
+                    pass
+
                 usernum = 0
                 usertxt = ""
-        try:
-            SPAM_CHATS.remove(message.chat.id)
-        except Exception:
-            pass
+
+    try:
+        SPAM_CHATS.remove(message.chat.id)
+    except Exception:
+        pass
 
 
 @app.on_message(
@@ -86,14 +111,21 @@ async def tag_all_users(_, message):
     & admin_filter
 )
 async def cancelcmd(_, message):
+
     chat_id = message.chat.id
+
     if chat_id in SPAM_CHATS:
+
         try:
             SPAM_CHATS.remove(chat_id)
         except Exception:
             pass
-        return await message.reply_text("**ᴛᴀɢɢɪɴɢ ᴘʀᴏᴄᴇss sᴜᴄᴄᴇssғᴜʟʟʏ sᴛᴏᴘᴘᴇᴅ!**")
+
+        return await message.reply_text(
+            "**ᴛᴀɢɢɪɴɢ ᴘʀᴏᴄᴇss sᴜᴄᴄᴇssғᴜʟʟʏ sᴛᴏᴘᴘᴇᴅ!**"
+        )
 
     else:
-        await message.reply_text("**ɴᴏ ᴘʀᴏᴄᴇss ᴏɴɢᴏɪɴɢ!**")
-        return
+        return await message.reply_text(
+            "**ɴᴏ ᴘʀᴏᴄᴇss ᴏɴɢᴏɪɴɢ!**"
+        )
